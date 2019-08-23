127457ec0269b592b7183bb5f2256279578ff2b6
Daniela Ichikawa <danichikawa@gmail.com>
2019-08-23 11:42:08 -0300

    Add dotenv - Protect my secret data in .env file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,6 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 import 'bootstrap';
+import improveDropdown from '../components/select';
+
+improveDropdown();
